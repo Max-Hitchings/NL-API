fc2ce7616678ec5202ab7548da936d5b27f8648f
gk41 <max.hitchings@gmail.com>
2021-03-18 16:44:21 +0000

    changed error to return error

--- routes/detect.js
+++ routes/detect.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
       res.status(200).json({ result: result });
     });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ error: err.message });
   }
 });
 
